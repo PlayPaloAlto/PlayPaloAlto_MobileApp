510787113859ab7b15b17b3571ca1d3082373d9a
iliogr <aris.ilio@gmail.com>
2014-06-10 19:58:51 -0700

    initial

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+/*
+ * Default Gruntfile for AppGyver Steroids
+ * http://www.appgyver.com
+ *
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks("grunt-steroids");
+
+  grunt.registerTask("default", ["steroids-make", "steroids-compile-sass"]);
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+};
---
+++ www/js/app.js
@@ -0,0 +1,68 @@
+angular.module('ionicApp', ['ionic'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('tabs', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "tabs.html"
+    })
+    .state('tabs.home', {
+      url: "/home",
+      views: {
+        'home-tab': {
+          templateUrl: "home.html",
+          controller: 'HomeTabCtrl'
+        }
+      }
+    })
+    .state('tabs.events', {
+      url: "/events",
+      views: {
+        'home-tab': {
+          templateUrl: "events.html"
+        }
+      }
+    })
+    .state('tabs.challenges', {
+      url: "/challenges",
+      views: {
+        'home-tab': {
+          templateUrl: "challenges.html"
+        }
+      }
+    })
+    .state('tabs.leaderboard', {
+      url: "/leaderboard",
+      views: {
+        'home-tab': {
+          templateUrl: "leaderboard.html"
+        }
+      }
+    })
+    .state('tabs.profile', {
+      url: "/profile",
+      views: {
+        'home-tab': {
+          templateUrl: "profile.html"
+        }
+      }
+    })
+    .state('tabs.coupons', {
+      url: "/coupons",
+      views: {
+        'home-tab': {
+          templateUrl: "coupons.html"
+        }
+      }
+    });
+
+
+   $urlRouterProvider.otherwise("/tab/home");
+
+})
+
+.controller('HomeTabCtrl', function($scope) {
+  console.log('HomeTabCtrl');
+});
